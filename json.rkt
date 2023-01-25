2aa174f3c6a6928a1905c3b6d379c8dea0b5ba3d
Mi1kDev <tjthemorgan@gmail.com>
2023-01-25 17:47:41 +0000

    Attempt at JSON saving and loading. Awaiting stackoverflow help

---
+++ json.rkt
@@ -0,0 +1,30 @@
+#lang racket
+(require json)
+
+(define x "hendon")
+(define b #hasheq(
+    (start_location . "hendon")
+    (end_location . "tottenham")
+    (ETA . 30)
+    (route . ("g"))
+    (optional_parameters . #hasheq((accessibility . #f)))
+))
+
+
+;(display a)
+;(hash-ref a "start_location")
+#|
+(with-output-to-string 
+    (lambda () (write-json b))
+)|#
+
+(define write-json-wrapper (lambda (jsexpr filename)
+    (call-with-output-file filename (lambda (x) (write-json jsexpr x)) #:exists 'replace)
+))  
+
+(define read-json-wrapper (lambda (filename)
+    (call-with-input-file filename read-json)
+))
+
+(write-json-wrapper b "save.json")
+;(read-json-wrapper "save.json")
